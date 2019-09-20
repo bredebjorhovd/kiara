81dc4b57bf16608bf2d4d368683a812931e1cc9e
BJORHOVD Brede <brede.bjorhovd@gmail.com>
2019-09-20 12:29:47 +0200

    Initial commit with pandas funcs and t-tests

---
+++ pandas_funcs.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def add_constant_index_level(df: pd.DataFrame, value: None, level_name=str):
+    return pd.concat([df], keys=[value], names=[level_name])
+
+def flatten_cols(df: pd.DataFrame, delim: str = ';'):
+    new_cols = [delim.join((col_lev for col_lev in tup if col_lev)) for tup in df.columns.values]
+    
+    ndf = df.copy()
+    ndf.columns = new_cols
+    
+    return ndf
+
+def unflatten_cols(df: pd.DataFrame, delim: str =';'):
+    new_cols = pd.MultiIndex.from_tuples([tuple(col.split(delim)) for col in df.columns])
+    
+    ndf = df.copy()
+    ndf.columns = new_cols
+    
+    return ndf
+
+def index_level_types(df):
+    return [f"{df.index.names[i]}: {df.index.get_level_values(n).dtype}"
+           for i, n in enumerate(df.index.names)]
---
+++ statistical_tests.py
@@ -0,0 +1,54 @@
+from numpy import mean
+from numpy import sqrt
+import numpy as np
+from scipy.stats import sem
+from scipy.stats import t
+from scipy import stats
+
+def independent_t_test(data1, data2, alpha):
+
+    mean1, mean2 = mean(data1), mean(data2)
+    # standard error
+    se1, se2 = sem(data1), sem(data2)
+    # squared error on difference between samples
+    sed = sqrt(se1**2.0 + se2**2.0)
+    # calculate t-statistic
+    t_stat = (mean1 - mean2) / sed
+    # degrees of freedom
+    df = len(data1) + len(data2) - 2
+    # calculate critical value
+    cv = t.ppf(1.0 - alpha, df)
+    # calculate p-value
+    p = (1.0 - t.cdf(abs(t_stat), df)) * 2.0
+
+    if alpha > p:
+        print('Reject')
+    else:
+        print('Accept')
+    return t_stat, df, cv, p
+
+def welch_t_test(data1, data2):
+    """
+    t, df, p = welch_t_test(data1, data2)
+    """
+    n1 = data1.size
+    n2 = data2.size
+    mu1 = np.mean(data1)
+    mu2 = np.mean(data2)
+    var1 = np.var(data1, ddof=1)
+    var2 = np.var(data2, ddof=2)
+    t = (mu1 - mu2) / np.sqrt(var1 / n1 + var2 / n2)
+    df = (var1 / n1 + var2 / n2)**2 / (var1**2 / (n1**2 * (n1 - 1)) + var2**2 / (n2**2 * (n2 - 1)))
+    p = 2 * stats.t.cdf(-abs(t), df)
+    
+    return t, df, p
+
+def p_value_interpreter(p, alpha):
+    """ Usage:
+    p_value_interpreter(p, alpha)
+    """
+    # interpret via p-value
+    if p > alpha:
+        print('Accept null hypothesis that the means are equal.')
+    else:
+        print('Reject the null hypothesis that the means are equal.')
