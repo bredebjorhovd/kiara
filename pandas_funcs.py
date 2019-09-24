0bd9ecefc244e97df75cd9dacfd2e7d76aa85e69
BJORHOVD Brede <brede.bjorhovd@gmail.com>
2019-09-24 12:45:56 +0200

    Added unique counts and fixed spacing

--- pandas_funcs.py
+++ pandas_funcs.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+
 def add_constant_index_level(df: pd.DataFrame, value: None, level_name=str):
     return pd.concat([df], keys=[value], names=[level_name])
+
 
 def flatten_cols(df: pd.DataFrame, delim: str = ';'):
     new_cols = [delim.join((col_lev for col_lev in tup if col_lev)) for tup in df.columns.values]
@@ -11,6 +13,7 @@ def flatten_cols(df: pd.DataFrame, delim: str = ';'):
     
     return ndf
 
+
 def unflatten_cols(df: pd.DataFrame, delim: str =';'):
     new_cols = pd.MultiIndex.from_tuples([tuple(col.split(delim)) for col in df.columns])
     
@@ -19,6 +22,14 @@ def unflatten_cols(df: pd.DataFrame, delim: str =';'):
     
     return ndf
 
+
 def index_level_types(df):
     return [f"{df.index.names[i]}: {df.index.get_level_values(n).dtype}"
            for i, n in enumerate(df.index.names)]
+
+
+def unique_counts(df):
+    for i in df.columns:
+        count = df[i].nunique()
+        print(i, ": ", count)
+
